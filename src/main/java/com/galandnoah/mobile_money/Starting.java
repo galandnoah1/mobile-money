8321a6a6f36c6a92079f298dfff27dbba66ef460
Galand NOAH <galandnoah@gmail.com>
2026-09-10 12:25:50 +0100

    fix: define Starting as a Spring bean

--- src/main/java/com/galandnoah/mobile_money/Starting.java
+++ src/main/java/com/galandnoah/mobile_money/Starting.java
@@ -2,7 +2,9 @@ package com.galandnoah.mobile_money;
 
 import org.jspecify.annotations.NonNull;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Starting implements CommandLineRunner {
     @Override
     public void run(String @NonNull ... args) throws Exception {
